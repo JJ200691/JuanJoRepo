fc512e3cab7186ece8db9110513f5b00a1f8acdc
Juan José Martinez <juanjomm71@gmail.com>
2022-04-26 16:42:04 -0300

    Subiendo cambios

--- JavaScript(Junior) - Dalto/cofla.js
+++ JavaScript(Junior) - Dalto/cofla.js
@@ -1,4 +1,4 @@
-/* Capitulo 1 */
+/* Capitulo 1: mostrar que helado puede comprar cada uno con el dinero que tienen */
 var nombre1 = "Roberto";
 var nombre2 = "Pedro";
 var nombre3 = "Cofla";
@@ -16,7 +16,75 @@ if (dinero3 == 3) {
     alert(`${nombre3} te alcanza para un helado con confites o un pote de 1/4`)
 }
 
-/* Capitulo 2 */
+/* Capitulo 2: 
+- En una fiesta crear una máquina que solo deje pasar a los mayores de edad y la 1er persona
+que entre después de las 2am no paga entrada. 
+- Por otro lado Cofla empezó las clases y tiene que crear un sistema para registrar ausentes(A) y presentes(P) 
+en clase, se puede tener un máximo del 10% de ausencias. Hay 20 alumnos y hay que medirlo en 30 días.
+- Crear una calculadora que haga todas las operaciones matemáticas. */
+function ingresarPersona() {
+    let nombre = prompt("Bienvenido a la fiesta! Cuál es tu nombre?");
+    let edad = prompt("Cuantos años tenés?");
+    let hora = prompt("Ingresa la hora");
+    if (edad >= 18 && hora >= 2) {
+        alert(`Felicitaciones ${nombre}, puedes ingresar y no pagás entrada`);
+    } else if (edad >= 18 && hora < 2) {
+        alert(`Podés ingresar ${nombre}, abonás tu entrada en taquilla`);
+    } else {
+        alert(`No sos mayor de 18 ${nombre}! No podés ingresar`);
+    }
+}
+ingresarPersona();
+
+var alumnos = [];
+var asistencia = [];
+var contP = 0;
+var contA = 0;
+ingresarAlumnos = () => {
+    for (i = 0; i < 20; i++) {
+        alumnos[i] = prompt("Ingrese el nombre del alumno");
+        document.write(alumnos[i]);
+        for (j = 0; j < 20; j++) {
+            asistencia[j] = prompt("Presente(P) o Ausente(A)");
+            if (asistencia[j] == "P") {
+                contP++;
+            } else {
+                contA++;
+            }
+            document.write(" " + asistencia[j]);
+        }
+        if (contA > contP) {
+            document.write(" | Ha reprobado por cantidad de inasistencias");
+        } else {
+            document.write(" | Aprobado");
+        }
+        document.write("<br>");
+    }
+}
+ingresarAlumnos();
+
+function calculadora(op, n1, n2) {
+    switch (op) {
+        case "suma": alert(n1 + n2);
+            break;
+        case "resta": alert(n1 - n2);
+            break;
+        case "multiplicacion": alert(n1 * n2);
+            break;
+        case "division": alert(n1 / n2);
+            break;
+        default:
+            break;
+    }
+}
+num1 = parseInt(prompt("Ingrese un número"));
+num2 = parseInt(prompt("Ingrese otro número"));
+operacion = prompt("Ingrese una operación");
+calculadora(operacion, num1, num2);
+
+
+
+
 
 
 
--- JavaScript(Junior) - Dalto/junior.js
+++ JavaScript(Junior) - Dalto/junior.js
@@ -1,21 +1,22 @@
-/* Tipos de variables */
+//  Tipos de variables 
+
 String = "hola como va";
 Number = 25;
 Boolean = true;
 
-/* Formas de declarar una variable */
+// Formas de declarar una variable
 var número = 25; // variable global 
 let cadena = "hola"; // variable local, solo se reconoce en un bloque de código 
 const Boolean = false; // variable constante, no puede cambiar su valor 
 
-/* Casos especiales */
+// Casos especiales
 let num = null; // la variable se inicializa intencionalmente con valor nulo
 let num2 = NaN; // not a number significa que la variable con la que queremos operar no es un numero
 
-/* Funcion prompt */
-let nombre = prompt("decime tu nombre"); // el nombre ingresado se almacena en 'nombre'
+// Funcion prompt
+let nombre = prompt("decime tu nombre"); // el nombre ingresado se almacena en 'nombre'(abrir consola al usuario)
 
-/* Operadores de asignación */
+// Operadores de asignación
 var n = 10;
 n += 5; // suma al valor de la variable
 n -= 4; // resta al valor de la variable
@@ -27,7 +28,7 @@ n++; // incrementa en 1
 n--; // decrementa en 1
 n ** 2; // eleva a la potencia según el número
 
-/* Operadores aritméticos */
+// Operadores aritméticos
 let num1 = 1;
 let nume2 = 2;
 num1 == nume2; // igualdad en valor (no diferencia tipo de dato)
@@ -39,14 +40,14 @@ num1 > nume2; // mayor
 num1 <= nume2; // menor o igual
 num1 >= nume2; // mayor o igual
 
-/* Operadores lógicos */
+// Operadores lógicos
 let valor = true;
 let valor2 = true;
 let resultado1 = valor && valor2; // operador "y" (devuelve true si ambas son true)
 let resultado2 = valor || valor2; // operador "o" (devuelve true si alguna de las 2 expresiones es true)
 let resultado3 = !valor2; //devuelve true si la expresion es falsa y viceversa
 
-/* Concatenación */
+// Concatenación
 var cad1 = "Hola";
 var cad2 = "como estás?";
 concaten = cad1 + cad2; // de esta forma concateno 2 cadenas
@@ -55,7 +56,7 @@ frase = cad1.concat(cad2); // método para concatenar
 frase2 = "Hola" + cad2 + " todo bien?"; // también se puede con signos +
 frase3 = `Hola ${cad2} todo bien`; // forma para evitar errores con backticks
 
-/* Condicionales */
+// Condicionales
 var algo = "JuanJo";
 if (algo == "JuanJo") {
     alert("hola" + algo);
@@ -65,19 +66,19 @@ if (algo == "JuanJo") {
     alert("chau");
 }
 
-/* Arrays */
-let array = ["valor","valor2",25, true]; // puede contener cualquier tipo de dato
+// Arrays
+let array = ["valor", "valor2", 25, true]; // puede contener cualquier tipo de dato
 document.write(array[2]); // para mostrar una posición específica (arranca de 0)
 let arrayAsociado = {   // se puede asociar y después se llama por el dato asociado en vez de la posición
     marca: "toyota",
     modelo: "corolla",
     año: 2018,
     tipo: "sedan"
-} 
+}
 document.write(arrayAsociado["marca"]);
 document.write(arrayAsociado["modelo"]);
 
-/* Bucles */
+// Bucles
 let n1 = 10;
 let n2 = 5;
 
@@ -104,5 +105,35 @@ for (aux in animales) {  //  muestra las posiciones de cada elemento
 for (aux of animales) {  //  muestra el contenido de cada elemento
     document.write(aux, "<br>");
 }
-
 // se puede utilizar la sentencia break o continue en cualquier bucle
+
+// Funciones
+function saludar() {    //  Se crea con la palabra reservada "function"
+    prompt("Hola, cómo estás?");
+    alert("Todo bien, vos?");
+}
+saludar();  //  llamado a la función
+
+function devolverEdad() {   //  función con retorno
+    let respuesta = prompt("Cuantos años tienes?");
+    return respuesta;
+}
+let func = devolverEdad();
+document.write(func);
+
+function suma(n1, n2) {     // funcion con parámetros
+    let suma = n1 + n2;
+    document.write(suma);
+}
+suma(5, 10);
+
+function resta(n1, n2) {    // funcion con parametros y retorno
+    return n1 - n2;
+}
+resta(10, 5);
+
+const multiplicar = (n1, n2) => n1 * n2;     // función flecha      
+multiplicar(5, 2);
+
+
+
